e22610ff91516f18ef68e83df437b0ef25f12071
garginr <60424868+garginr@users.noreply.github.com>
2022-01-12 22:14:41 +0530

    Add files via upload

---
+++ calendar.js
@@ -0,0 +1,67 @@
+  var dt=new Date()
+
+  function RenderDate(){
+    dt.setDate(1);
+    var day=dt.getDay();
+    console.log(dt.getDay());
+    var endDate=new Date(
+    
+        dt.getFullYear(),
+        dt.getMonth() +1,
+        0
+    ).getDate();
+    
+    var prevDate=new Date(
+    
+        dt.getFullYear(),
+        dt.getMonth(),
+        0
+        ).getDate();
+    var today=new Date().getDate();
+    console.log(today);
+    var months=[
+        "January",
+        "February",
+        "March",
+        " April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "september",
+        "October",
+        "November",
+        "December"  
+    ];
+    
+    document.getElementById("date_str").innerHTML= dt.toDateString();
+    document.getElementById("month").innerHTML= months[dt.getMonth()];
+    
+    var cells="";
+    
+    for(x=day;x>0;x--){
+        cells+="<div class='prev_date'>"+(prevDate-x+1) +"</div>"
+    }
+    for(i=1;i<=endDate;i++){
+        if(i==today){
+            cells+= "<div class='today'>"+ i +"</div>"  
+        
+    }else {
+        cells+= "<div>"+ i +"</div>"
+    }
+}
+     document.getElementsByClassName("Days")[0].innerHTML=cells;
+
+  }
+
+function moveDate (para){
+    if(para=='prev'){
+        dt.setMonth(dt.getMonth()-1)
+    }
+    else if(para=='next'){
+        dt.setMonth(dt.getMonth()+1)
+    }
+        RenderDate();
+    }
+
+
